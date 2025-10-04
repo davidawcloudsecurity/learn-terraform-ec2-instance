cc05406544ff95b98169f5dd2c271c298dc2f539
daw <admin2@DESKTOP-1HNHK2U.localdomain>
2025-10-04 21:40:48 +0800

    fix: attach SSM managed policy to EC2 IAM role

--- main.tf
+++ main.tf
@@ -104,6 +104,11 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
