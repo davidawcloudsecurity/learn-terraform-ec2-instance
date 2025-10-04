c608fbe0b53a3d8293e1282b80a4068707b37a5a
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-10-04 20:29:45 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,9 @@
-bucket         = "learn-terraform-ec2-instance-terraform-state"
-key            = "lamp-stack/terraform.tfstate"
-region         = "us-east-1"
-use_lockfile   = true
-#    dynamodb_table = "terraform-locks"
-#    encrypt        = true
+terraform {
+  backend "s3" {
+    bucket         = "learn-terraform-ec2-instance-terraform-state"
+    key            = "project1/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
