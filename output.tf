a8c374b0c64643a41514d7e724b2ca5f86c8c5da
daw <admin2@DESKTOP-1HNHK2U.localdomain>
2025-10-05 14:41:04 +0800

    feat: add public IP and S3 bucket name to terraform outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "instanceid" {
   value = aws_instance.ec2.*.id
 }
+
+output "public_ip" {
+  value = aws_instance.ec2.*.public_ip
+}
+
+output "s3_bucket" {
+  value = aws_s3_bucket.images.bucket
+}
