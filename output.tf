49b673e7298b327d72e2962cd87ef4c14ffda68f
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-10-12 18:22:24 +0800

    Update output.tf - comment s3

--- output.tf
+++ output.tf
@@ -5,7 +5,8 @@ output "instanceid" {
 output "public_ip" {
   value = aws_instance.ec2.*.public_ip
 }
-
+/* put back if using s3 main.tf
 output "s3_bucket" {
   value = aws_s3_bucket.images.bucket
 }
+*/
