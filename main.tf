456f1b535996e9d85fa62a387086add558a91638
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-10-12 18:38:54 +0800

    Update main.tf - added instance profile

--- main.tf
+++ main.tf
@@ -86,12 +86,40 @@ resource "aws_security_group" "default" {
   }
 }
 
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
 resource "aws_instance" "ec2" {
   count                  = local.instance_number
   instance_type          = var.instance_type
   ami                    = data.aws_ami.ami.image_id
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.default.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   user_data              = base64encode(file("${path.module}/userdata.sh"))
 }
 
